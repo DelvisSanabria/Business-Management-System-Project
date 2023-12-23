08eb1e514a6366b31f0d4b3d27cd131f8b69ca66
delvis Sanabria <134345133+DelvisSanabria@users.noreply.github.com>
2023-12-23 17:49:58 -0400

    require Changes

---
+++ FrontEnd/business-management-system-project/src/pages/Pages.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable react/prop-types */
+import Layout from './components/Layout'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login'
+import Products from './pages/Products.jsx'
+import AddProduct from './pages/AddProduct.jsx'
+import EditProduct from './pages/EditProduct.jsx'
+import ViewProduct from './pages/ViewProduct.jsx'
+import Users from './pages/Users.jsx'
+import AddUser from './pages/AddUser.jsx'
+import EditUser from './pages/EditUser.jsx'
+import Customers from './pages/Customers.jsx'
+import AddCustomer from './pages/AddCustomer.jsx'
+import EditCustomer from './pages/EditCustomer.jsx'
+import Vendors from './pages/Vendors.jsx'
+import AddVendor from './pages/AddVendor.jsx'
+import EditVendor from './pages/EditVendor.jsx'
+import Sales from './pages/Sales.jsx'
+import AddSale from './pages/AddSale.jsx'
+import EditSale from './pages/EditSale.jsx'
+import Settings from './pages/Settings.jsx'
+import Contact from './pages/ContactUs.jsx'
+import Reports from './pages/Reports.jsx'
+import Cart from './pages/Cart.jsx'
+import NoPage from './pages/NoPage.jsx'
+import Register from './pages/Register.jsx'
+import Profile from './pages/Profile.jsx'
+import MakeSale from './pages/MakeSale.jsx'
+
+export default {
+  Layout,
+  Home,
+  Login,
+  Products,
+  AddProduct,
+  EditProduct,
+  ViewProduct,
+  Users,
+  AddUser,
+  EditUser,
+  Customers,
+  AddCustomer,
+  EditCustomer,
+  Vendors,
+  AddVendor,
+  EditVendor,
+  Sales,
+  AddSale,
+  EditSale,
+  Settings,
+  Contact,
+  Reports,
+  Cart,
+  NoPage,
+  Register,
+  Profile,
+  MakeSale,
+}
