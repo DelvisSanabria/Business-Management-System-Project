39dc3b6772cbb192aa7a50258ae64db94cebe9be
SergioDV <134448590+SergioDV95@users.noreply.github.com>
2023-12-24 06:03:15 -0400

    Inicio del frontend
    Lo que llevo del navbar versión mobile

---
+++ FrontEnd/business-management-system-project/src/Session/session.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const Session = createContext();
+
+export default Session;
---
+++ FrontEnd/business-management-system-project/src/components/exports.js
@@ -0,0 +1,20 @@
+export { default as Session } from '../Session/session';
+export { default as Layout } from './Layout(2)';
+/*export { default as SignUp } from './SignUp';
+export { default as ProductForm } from './ProductForm';
+export { default as ProductsDashboard } from './ProductsDashboard';
+export { default as LogIn } from './LogIn';
+export { default as Home } from './Home';
+export { default as Cart } from './Cart';
+export { default as Profile } from './Profile';
+export { default as ProductsDisplay } from './ProductsDisplay';*/
+
+//images
+import polar from "../images/various/EmpresasPolar2010.png"
+import menu from "../images/icons/menu.png"
+import home from "../images/icons/home.png"
+import box from "../images/icons/box.png"
+import login from "../images/icons/login.png"
+import signup from "../images/icons/add-user.png"
+
+export { polar, menu, home, box, login, signup };
