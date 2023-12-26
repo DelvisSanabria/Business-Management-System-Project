6446d89cd0e2257de2110726efb10fcfdc6d3b5f
SergioDV <134448590+SergioDV95@users.noreply.github.com>
2023-12-26 11:03:30 -0400

    Updates 26/12

--- FrontEnd/business-management-system-project/src/components/exports.js
+++ FrontEnd/business-management-system-project/src/components/exports.js
@@ -1,20 +1,23 @@
 export { default as Session } from '../Session/session';
-export { default as Layout } from './Layout(2)';
-/*export { default as SignUp } from './SignUp';
-export { default as ProductForm } from './ProductForm';
-export { default as ProductsDashboard } from './ProductsDashboard';
+export { default as SignUp } from './SignUp';
 export { default as LogIn } from './LogIn';
-export { default as Home } from './Home';
-export { default as Cart } from './Cart';
-export { default as Profile } from './Profile';
-export { default as ProductsDisplay } from './ProductsDisplay';*/
 
 //images
-import polar from "../images/various/EmpresasPolar2010.png"
-import menu from "../images/icons/menu.png"
-import home from "../images/icons/home.png"
-import box from "../images/icons/box.png"
-import login from "../images/icons/login.png"
-import signup from "../images/icons/add-user.png"
+import polar from "../images/various/EmpresasPolar2010.png";
+import menuIco from "../images/icons/menu.png";
+import home from "../images/icons/home.png";
+import box from "../images/icons/box.png";
+import log_in from "../images/icons/login.png";
+import signup from "../images/icons/add-user.png";
+import shopping_bag from "../images/icons/shopping-bag.png";
+import cart from "../images/icons/Cart.png";
+import log_out from "../images/icons/log-out.png";
+import settings from "../images/icons/settings.png";
+import userIco from "../images/icons/user.png";
+import camera from "../images/icons/camera.png";
+import blueBg from "../images/various/bg.png";
+import bg_login from "../images/various/bg_login.png";
+import polarSede from "../images/various/Sede_Polar.jpeg";
 
-export { polar, menu, home, box, login, signup };
+
+export { polar, menuIco, home, box, log_in, signup, shopping_bag, cart, log_out, settings, userIco, camera, blueBg, bg_login, polarSede };
