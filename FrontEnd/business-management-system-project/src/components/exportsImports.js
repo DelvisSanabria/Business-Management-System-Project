973198dbc445315309e36e0e51c15346214d5330
SergioDV95 <134448590+SergioDV95@users.noreply.github.com>
2024-01-10 04:15:40 -0400

    Update exportsImports.js

--- FrontEnd/business-management-system-project/src/components/exportsImports.js
+++ FrontEnd/business-management-system-project/src/components/exportsImports.js
@@ -1,6 +1,6 @@
 export { default as Session } from '../Session/session';
 export { default as SignUp } from '../pages/SignUp';
-export { default as LogIn } from '../pages/Login';
+export { default as LogIn } from '../pages/LogIn';
 export { default as ProductForm } from '../pages/ProductForm';
 export { default as ProductsDashboard } from '../pages/ProductsDashboard';
 export { default as CartModal } from '../pages/CartModal';
