6edb4a350648cfc601d455beae6f7fedcbaf59f9
SergioDV95 <134448590+SergioDV95@users.noreply.github.com>
2024-01-18 02:46:56 -0400

    Actualización factura

--- FrontEnd/business-management-system-project/src/pages/Bill.jsx
+++ FrontEnd/business-management-system-project/src/pages/Bill.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useContext } from "react";
 import { useLocation } from "react-router-dom";
 import { Session } from "../Session/session";
 import axios from "axios";
+import { blueBg } from "./../components/exportsImports";
 
 export default function Bill() {
    const { user } = useContext(Session);
@@ -38,14 +39,17 @@ export default function Bill() {
       for(let doc of products) {
          for(let id in quantity) {
             if(doc._id === id) {
-               productsList.push({name: doc.name, quantity: quantity[id]});
+               productsList.push({name: doc.name, quantity: quantity[id], price: doc.price});
             }
          }
       }
-      return productsList.map(({name, quantity}, index) => (
-         <span key={index} className="flex w-full gap-[15px] lg:justify-between">
-            <span>{name}</span>
-            <span className="font-medium">x{quantity}</span>
+      return productsList.map(({name, quantity, price}, index) => (
+         <span key={index} className="flex w-full gap-[15px] justify-between">
+            <span className="flex flex-col">
+               <span className="font-light">{name}</span>
+               <span className="font-medium text-[0.8em]">x{quantity}</span>
+            </span>
+            <span className="text-[#9098B1] text-[0.8em]">${price.toFixed(2)}</span>
          </span>
       ));
    }
@@ -59,38 +63,44 @@ export default function Bill() {
    }
 
    return (
-      <section className="bg-[#D9D9D9] w-full flex justify-center items-center">
-      <h4 className="text-[#14274E] text-[20px]">Tu Factura</h4>
-      <div className="flex flex-col p-[15px] w-[80%] lg:w-[50%]">
-         <div className="flex justify-between">
-            <p>Referencia:</p>
-            <p>{saleData._id}</p>
+      <section className="relative bg-[#1A3365] w-full flex flex-col gap-[25px] h-screen py-[50px] items-center">
+         <h4 className="text-[#E7E7E7] text-[20px]">Tu Factura</h4>
+         <div className="flex z-10 flex-col p-[15px] w-[80%] gap-[15px] lg:w-[50%] bg-[#F9F9F9] rounded-[12px] ">
+            <div className="flex justify-between items-center pb-[15px] border-b-[1px] border-[#E7E7E7] ">
+               <p>Referencia:</p>
+               <p className="font-light max-w-[50%] max-lg:overflow-x-auto">{saleData._id}</p>
+            </div>
+            <div className="flex justify-between items-center pb-[15px] border-b-[1px] border-[#E7E7E7] ">
+               <p>Fecha:</p>
+               <p className="font-light">{renderTime(saleData.createdAt)}</p>
+            </div>
+            <div className="flex justify-between items-center pb-[15px] border-b-[1px] border-[#E7E7E7] ">
+               <p>Cliente:</p>
+               <p className="font-light">{user.name + " " + user.lastName}</p>
+            </div>
+            <div className="flex flex-col">
+               <div className="flex justify-between items-center pb-[15px]">
+                  <p>Productos:</p>
+                  <p>Precio:</p>
+               </div>
+               <div className="flex justify-between items-center pb-[15px] border-b-[1px] border-[#E7E7E7] ">
+                  <p className="w-full">{productData && renderProducts(productData, saleData.quantity)}</p>
+               </div>
+            </div>
+            <div className="flex justify-between items-center pb-[15px] border-b-[1px] border-[#E7E7E7] ">
+               <p>Subtotal:</p>
+               <p>${saleData.subtotal && saleData.subtotal.toFixed(2)}</p>
+            </div>
+            <div className="flex justify-between items-center pb-[15px] border-b-[1px] border-[#E7E7E7] ">
+               <p>IVA 16%:</p>
+               <p>${saleData.tax && saleData.tax.toFixed(2)}</p>
+            </div>
+            <div className="flex justify-between items-center pb-[15px] text-[#223263] text-[1.1em] font-bold">
+               <p>Total:</p>
+               <p>${saleData.total && saleData.total.toFixed(2)}</p>
+            </div>
          </div>
-         <div className="flex justify-between">
-            <p>Fecha:</p>
-            <p>{renderTime(saleData.createdAt)}</p>
-         </div>
-         <div className="flex justify-between">
-            <p>Cliente:</p>
-            <p>{user.name}</p>
-         </div>
-         <div className="flex justify-between">
-            <p>Productos:</p>
-            <p>{productData && renderProducts(productData, saleData.quantity)}</p>
-         </div>
-         <div className="flex justify-between text-[#9098B1] text-[0.8em]">
-            <p>Subtotal:</p>
-            <p>{saleData.subtotal}</p>
-         </div>
-         <div className="flex justify-between text-[#9098B1] text-[0.8em]">
-            <p>IVA 16%:</p>
-            <p>{saleData.tax}</p>
-         </div>
-         <div className="flex justify-between text-[#223263] font-bold">
-            <p>Total:</p>
-            <p>{saleData.total}</p>
-         </div>
-      </div>
+         <img className="z-0 absolute w-full h-[70%] bottom-0 lg:hidden" src={blueBg} alt="background" />
       </section>
   );
 }
